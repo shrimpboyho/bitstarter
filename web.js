e4f42f5c8fcfbd4b08601a7e74ee7d3a80d9827c
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-07-04 13:32:08 -0400

    Serving html file

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express.createServer();
+
+app.configure(function(){
+	app.use(express.static(__dirname + '/public'));
+});
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	res.sendfile(__dirname + '/index.html');;
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  	console.log("Listening on " + port);
 });
+
+
